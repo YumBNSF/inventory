c7cd138c262704a1a5c1cf19f8127e962438b65f
Malik <willis.malik.r@gmail.com>
2023-05-22 12:24:45 -0500

    added Item's components, Item Model, Routes, views,

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -3,31 +3,46 @@ import { SaucesList } from './SaucesList';
 
 // import and prepend the api url to any fetch calls
 import apiURL from '../api';
+import {ItemList} from "./ItemList";
 
 export const App = () => {
 
-	const [sauces, setSauces] = useState([]);
+	// const [sauces, setSauces] = useState([]);
+	const [items, setItems] = useState([]);
 
-	async function fetchSauces(){
+	// async function fetchSauces(){
+	// 	try {
+	// 		const response = await fetch(`${apiURL}/sauces`);
+	// 		const saucesData = await response.json();
+	//
+	// 		setSauces(saucesData);
+	// 	} catch (err) {
+	// 		console.log("Oh no an error! ", err)
+	// 	}
+	// }
+	async function fetchItems(){
 		try {
-			const response = await fetch(`${apiURL}/sauces`);
-			const saucesData = await response.json();
-			
-			setSauces(saucesData);
+			const response = await fetch(`${apiURL}/items`);
+			const itemData = await response.json();
+
+			setItems(itemData);
 		} catch (err) {
 			console.log("Oh no an error! ", err)
 		}
 	}
 
 	useEffect(() => {
-		fetchSauces();
+		fetchItems();
 	}, []);
 
 	return (
 		<main>	
-      <h1>Sauce Store</h1>
-			<h2>All things 🔥</h2>
-			<SaucesList sauces={sauces} />
+      {/*<h1>Sauce Store</h1>*/}
+		{/*	<h2>All things 🔥</h2>*/}
+		{/*	<SaucesList sauces={sauces} />*/}
+			<h1>Inventory</h1>
+			<h2>All Items</h2>
+			<ItemList items={items}/>
 		</main>
 	)
 }
