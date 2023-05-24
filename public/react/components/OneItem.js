58ec050ee85b4d7f9e9a22e503ccfd19baf414cf
Malik <willis.malik.r@gmail.com>
2023-05-24 15:29:39 -0500

    created dropdown with delete and edit functionality

--- public/react/components/OneItem.js
+++ public/react/components/OneItem.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import apiURL from "../api";
 
 export const OneItem = ({itemReturn}) => {
-    
+    // for dropdown
+    const [open, setOpen] = useState(false);
+    const [itemsBeenDeleted, setItemsBeenDeleted] = useState(false)
 
-    if(!itemReturn.id)
-    {
-        return <></>
+    // onclick function for the delete button
+    async function deleteSingleItem(itemId){
+        await fetch(`${apiURL}/${itemId}`, {
+            method: 'DELETE'
+        });
+        // sets useState to true if the button is clicked
+        setItemsBeenDeleted(true);
     }
 
-    else
-    {
+    // onclick function for dropdown
+    const handleOpen = () => {
+        setOpen(!open);
+    };
+    
+    // determines if there is an item by targeting item id, b/c all items have id
+    if(!itemReturn.id){
+        return <></>
+    // once item has been deleted variable is going to be true
+    }else if(itemsBeenDeleted){
+        return <>Item is successfully Deleted!</>
+    } else {
         return (
             <>
                 <div className = "products">
@@ -18,9 +35,24 @@ export const OneItem = ({itemReturn}) => {
                     <h2>{itemReturn.title}</h2>
                     <p>{itemReturn.description}</p>
                     <h3>${itemReturn.price}</h3>
+                    <div>
+                        <button onClick={handleOpen}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                 className="bi bi-three-dots" viewBox="0 0 16 16">
+                                <path
+                                    d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
+                            </svg>
+                        </button>
+                        {open ? (
+                            <div>
+                                <button onClick={()=>deleteSingleItem(itemReturn.id)}>Delete</button>
+                                <button onClick={()=>deleteSingleItem(itemReturn.id)}>Edit</button>
+                            </div>
+                            ):(
+                            <div></div>)}
+
+                    </div>
                 </div>
-    
-             
             </>
         )
     }
